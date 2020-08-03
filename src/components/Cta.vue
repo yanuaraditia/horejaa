7e5377e01e1aed05859b17623e6b4a2c5663f1bf
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-03 17:33:05 +0700

    Mobile text centered

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,9 +1,15 @@
 <template>
-    <section class="hero is-primary has-text-centered">
+    <section class="hero is-primary has-text-centered-mobile">
         <div class="hero-body">
-            <p>DISCOVER HOW TO REDUCE TRANSIT TIMES AND SHIPPING COSTS.</p>
-            <h2 class="title is-2">The Guide to Offering Affordable 2-Day Shipping</h2>
-            <button class="button is-white is-outlined is-rounded">Download Guide</button>
+            <div class="columns">
+                <div class="column is-8-desktop">
+                    <p>DISCOVER HOW TO REDUCE TRANSIT TIMES AND SHIPPING COSTS.</p>
+                    <h2 class="title is-2">The Guide to Offering Affordable 2-Day Shipping</h2>
+                </div>
+                <div class="column is-4-desktop">
+                    <button class="button is-white is-outlined is-rounded">Download Guide</button>
+                </div>
+            </div>
         </div>
     </section>
 </template>
