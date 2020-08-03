430bee880a83856c5b1c2000a016837ebd0426b8
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-03 17:35:19 +0700

    RET

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,12 +1,12 @@
 <template>
-    <section class="hero is-primary has-text-centered-mobile">
+    <section class="hero is-primary has-text-centered">
         <div class="hero-body">
             <div class="columns">
-                <div class="column is-8-desktop">
+                <div class="column is-8-desktop has-text-left-desktop">
                     <p>DISCOVER HOW TO REDUCE TRANSIT TIMES AND SHIPPING COSTS.</p>
                     <h2 class="title is-2">The Guide to Offering Affordable 2-Day Shipping</h2>
                 </div>
-                <div class="column is-4-desktop">
+                <div class="column is-4-desktop has-text-right-desktop">
                     <button class="button is-white is-outlined is-rounded">Download Guide</button>
                 </div>
             </div>
