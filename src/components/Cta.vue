b5f09feee11a5f6ffa3541758648d9bdb401b45f
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-03 17:37:04 +0700

    Flexibles

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,13 +1,15 @@
 <template>
     <section class="hero is-primary has-text-centered">
         <div class="hero-body">
-            <div class="columns">
-                <div class="column is-8-desktop has-text-left-desktop">
-                    <p>DISCOVER HOW TO REDUCE TRANSIT TIMES AND SHIPPING COSTS.</p>
-                    <h2 class="title is-2">The Guide to Offering Affordable 2-Day Shipping</h2>
-                </div>
-                <div class="column is-4-desktop has-text-right-desktop">
-                    <button class="button is-white is-outlined is-rounded">Download Guide</button>
+            <div class="container is-0-mobile">
+                <div class="columns">
+                    <div class="column is-8-desktop has-text-left-desktop">
+                        <p>DISCOVER HOW TO REDUCE TRANSIT TIMES AND SHIPPING COSTS.</p>
+                        <h2 class="title is-2">The Guide to Offering Affordable 2-Day Shipping</h2>
+                    </div>
+                    <div class="column is-4-desktop has-text-right-desktop">
+                        <button class="button is-white is-outlined is-rounded">Download Guide</button>
+                    </div>
                 </div>
             </div>
         </div>
