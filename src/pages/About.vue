f64eb33f79a7b3793f3f50e2aea8655d06a7d0ad
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-03 13:53:19 +0700

    Srer

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,7 +1,11 @@
 <template>
   <Layout>
-    <h1>About us</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <section class="section">
+      <div class="container is-fluid">
+        <h1 class="title is-1">About us</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      </div>
+    </section>
   </Layout>
 </template>
 
