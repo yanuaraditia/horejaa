6c1f46d4082bd134953866edf08ededc0f741199
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-05 16:56:43 +0700

    Mulai jos

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -16,8 +16,8 @@
                                 <b-field label="Nama Lengkap">
                                     <b-input
                                      v-model="form.fullName"
-                                     minlength="5"
                                      maxlength="70"
+                                     minlength="5"
                                      type="text"></b-input>
                                 </b-field>
                                 <div class="columns is-flex">
@@ -32,7 +32,11 @@
                                     </div>
                                     <div class="column is-6">
                                         <b-field label="Nomor Telepon">
-                                            <b-input v-model="form.phone" maxlength="70" type="text"></b-input>
+                                            <b-input
+                                             v-model="form.phone"
+                                             maxlength="70"
+                                             minlength="5"
+                                             type="text"></b-input>
                                         </b-field>
                                     </div>
                                 </div>
@@ -63,12 +67,14 @@ export default {
                 phone: null,
                 compName: null,
                 message: null
-            }
+            },
         }
+    },
+    metaInfo: {
+        title: 'Contact Us'
     },
     methods: {
         send() {
-            this.isSend = false
             if(
                 this.form.fullName == null ||
                 this.form.email == null ||
@@ -76,12 +82,31 @@ export default {
                 this.form.compName == null ||
                 this.form.message == null
             ) {
-                alert('All field must be filled')
+                this.danger('Cok isi sek')
                 return
             }
+            this.isSend = false
             setInterval(() => {
                 this.isSend = true
-            }, 1000);
+            }, 1000)
+
+            this.success('Joss')
+        },
+        danger(title) {
+            this.$buefy.toast.open({
+                duration: 500,
+                message: title,
+                position: 'is-bottom',
+                type: 'is-danger'
+            })
+        },
+        success(title) {
+            this.$buefy.toast.open({
+                duration: 500,
+                message: title,
+                position: 'is-bottom',
+                type: 'is-success'
+            })
         }
     }
 }
