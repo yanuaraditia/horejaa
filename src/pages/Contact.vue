e6200d3c7be513e1ccffbbe3a53c45b6513e0995
Yanuar Aditia <yanuaraditia@outlook.com>
2020-08-05 16:41:22 +0700

    validation

--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -14,25 +14,33 @@
                         <div class="column is-12-mobile is-6-desktop">
                             <form>
                                 <b-field label="Nama Lengkap">
-                                    <b-input maxlength="70" type="text"></b-input>
+                                    <b-input
+                                     v-model="form.fullName"
+                                     minlength="5"
+                                     maxlength="70"
+                                     type="text"></b-input>
                                 </b-field>
                                 <div class="columns is-flex">
                                     <div class="column is-6">
                                         <b-field label="E-mail">
-                                            <b-input maxlength="70" type="text"></b-input>
+                                            <b-input
+                                             v-model="form.email"
+                                             maxlength="70"
+                                             minlength="5"
+                                             type="email"></b-input>
                                         </b-field>
                                     </div>
                                     <div class="column is-6">
                                         <b-field label="Nomor Telepon">
-                                            <b-input maxlength="70" type="text"></b-input>
+                                            <b-input v-model="form.phone" maxlength="70" type="text"></b-input>
                                         </b-field>
                                     </div>
                                 </div>
                                 <b-field label="Nama Perusahaan">
-                                    <b-input maxlength="70" type="text"></b-input>
+                                    <b-input maxlength="70" v-model="form.compName" type="text"></b-input>
                                 </b-field>
                                 <b-field label="Message">
-                                    <b-input maxlength="200" type="textarea"></b-input>
+                                    <b-input maxlength="200" v-model="form.message" type="textarea"></b-input>
                                 </b-field>
                                 <button class="button is-primary is-fullwidth is-medium" v-on:click="send" v-if="isSend" type="button">Contact Us</button>
                                 <b-button class="is-medium is-fullwidth is-primary" v-else loading>Contact Us</b-button>
@@ -48,12 +56,29 @@
 export default {
     data() {
         return {
-            isSend: true
+            isSend: true,
+            form: {
+                fullName: null,
+                email: null,
+                phone: null,
+                compName: null,
+                message: null
+            }
         }
     },
     methods: {
         send() {
             this.isSend = false
+            if(
+                this.form.fullName == null ||
+                this.form.email == null ||
+                this.form.phone == null ||
+                this.form.compName == null ||
+                this.form.message == null
+            ) {
+                alert('All field must be filled')
+                return
+            }
             setInterval(() => {
                 this.isSend = true
             }, 1000);
