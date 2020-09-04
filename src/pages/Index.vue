9cb9366f289840d9f1040732f007b3573359727f
Yanuar Aditia <yanuaraditia@outlook.com>
2020-09-04 22:09:03 +0700

    STE

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -39,8 +39,8 @@
             <h5 class="subtitle is-6">Akurasi tinggi</h5>
           </div>
           <div class="column is-6-mobile is-4-desktop has-text-centered">
-            <h3 class="title is-1">3.500+</h3>
-            <h5 class="subtitle is-6">Produk telah tersalurkan</h5>
+            <h3 class="title is-1">98%</h3>
+            <h5 class="subtitle is-6">Integrasi sistem</h5>
           </div>
           <div class="column is-6-mobile is-4-desktop has-text-centered">
             <h3 class="title is-1">35+</h3>
